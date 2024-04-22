5cb259be7c1aed58570ec2bf31c1288ccbac1640
PavelSlimBilek <bilekpavel.p@gmail.com>
2024-04-22 21:46:49 +0200

    add example controller

---
+++ src/Controller/ExampleController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ExampleController extends AbstractController
+{
+
+    #[Route('/hello', name: 'hello')]
+    public function hello() : Response
+    {
+        return new Response("Hello World!");
+    }
+
+    #[Route('/hello/{name}', name: 'hello_param')]
+    public function helloParam($name) : Response
+    {
+        return new Response("Hello {$name}!");
+    }
+
+}
