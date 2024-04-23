2fab2b37cd2c80aa355575ece62928cb4948b4a6
PavelSlimBilek <bilekpavel.p@gmail.com>
2024-04-23 17:31:07 +0200

    add help.txt - commands

--- migrations/Version20240423143447.php
+++ migrations/Version20240423143447.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240423141832 extends AbstractMigration
+final class Version20240423143447 extends AbstractMigration
 {
     public function getDescription(): string
     {
